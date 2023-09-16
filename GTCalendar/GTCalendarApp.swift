4113fb6cb61597f29df21c64b5eb70b952d54d62
Gaurav Tak <gaurav.tak@roro.io>
2023-09-16 13:15:10 +0530

    Initial Commit

---
+++ GTCalendar/GTCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTCalendarApp.swift
+//  GTCalendar
+//
+//  Created by Roro Solutions LLP on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct GTCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
